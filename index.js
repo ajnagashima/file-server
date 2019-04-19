75453ad1d0e3631278ca93217b670a2b0be636fe
ajnagashima <arthurjnagashima@gmail.com>
2019-04-19 16:59:51 -0400

    File server now supports upload and file viewing
    Need to add react code to allow for upload and download
    
    **NOTE: uploading an empty file or a file that doesnt exist will cause
    the server to crash

--- index.js
+++ index.js
@@ -1,30 +1,60 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+const IncomingForm = require('formidable').IncomingForm
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+var fs = require('fs');
+var fileDir = './files';
+
+//TODO Replace with db 
+if (!fs.existsSync(fileDir)) {
+  console.log("Making file Directory");
+  fs.mkdirSync(fileDir);
+}
+
+var corsOptions = {
+  origin: '*',
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.post('/api/upload', (req, res) => {
-  //logic about uploading items
-  res.json(['upload']);
-  console.log("POST: upload");
+    var form = new IncomingForm()
+  
+
+    form.on('fileBegin', (field, file) => {
+      console.log("File upload Started")
+      file.path = fileDir + "/" + file.name
+    })
+
+    form.on('file', (field, file) => {
+      console.log('file finished')
+    })
+    form.on('end', () => {
+      res.json();
+    })
+  
+    form.parse(req)
+    console.log("POST: upload");
 });
 
 app.get('/api/download', (req, res) => {
-  //logic about fetching item urls for download
   res.json(['download']);
   console.log("GET: download")
 });
 
 app.get('/api/download/getFiles', (req, res) => {
-  res.json(['file1', 'file2', 'file3']);
+  fs.readdir(fileDir, (err, files) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    res.json(files)
+  })
   console.log("GET: downloadable Files");
-});
-
-// Redirect users to main page if requests dont match any of the above
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
